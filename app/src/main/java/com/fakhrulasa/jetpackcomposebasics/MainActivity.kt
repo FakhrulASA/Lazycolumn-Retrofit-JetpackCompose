18d93f8d4f83dff8bd935745dd6246de7c52eacc
fakhrulasa <fakhrulasa@gmail.com>
2021-11-09 00:51:40 +0600

    -topbar added -coroutine added

--- app/src/main/java/com/fakhrulasa/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/fakhrulasa/jetpackcomposebasics/MainActivity.kt
@@ -4,10 +4,10 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -19,39 +19,47 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.fakhrulasa.jetpackcomposebasics.network.Retrofitlient
 import com.fakhrulasa.jetpackcomposebasics.network.PostData
-import com.fakhrulasa.jetpackcomposebasics.ui.MainViewModel
+import com.fakhrulasa.jetpackcomposebasics.network.Retrofitlient
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
-    lateinit var PostData:List<PostData>
-    var viewModel: MainViewModel by viewModels()
+    lateinit var postData: List<PostData>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        CoroutineScope(Dispatchers.IO).launch {
+            postData = Retrofitlient.api.getPost()
+            withContext(Dispatchers.Main) {
+                setContent {
+                    MyApp(postData)
+                }
+            }
 
-        setContent {
-            MyApp(PostData)
         }
+
+
     }
 }
 
 @Composable
-private fun MyApp(PostData:List<PostData>) {
+private fun MyApp(postData: List<PostData>) {
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
         OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
     } else {
-        Greetings(PostData)
+        Greetings(postData)
     }
 }
 
@@ -60,41 +68,73 @@ private fun OnboardingScreen(onContinueClicked: () -> Unit) {
     Surface {
         Column(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Welcome to the Basics Codelab!")
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick = onContinueClicked
+            TopAppBar(
+                title = { Text(text = "Squishy Post") },
+                backgroundColor = colorResource(id = R.color.purple_500),
+                contentColor = Color.White
+            )
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
             ) {
-                Text("Continue")
+                Text("Let's see the post's")
+                Button(
+                    modifier = Modifier
+                        .padding(20.dp)
+                        .background(colorResource(id = R.color.purple_500)),
+                    onClick = onContinueClicked,
+
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black)
+                ) {
+
+                    Text(
+                        text = "CHECKOUT POST",
+                        color = Color.White
+                    )
+                }
             }
+
         }
     }
 }
 
 @Composable
 private fun Greetings(names: List<PostData>) {
-    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-        items(items = names) { name ->
-            Greeting(name = name.title, msg=name.body,id=name.id)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TopAppBar(
+            title = { Text(text = "Squishy Post") },
+            backgroundColor = colorResource(id = R.color.purple_500),
+            contentColor = Color.White
+        )
+        LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+            items(items = names) { name ->
+                Greeting(name = name.title, msg = name.body, id = name.id)
+            }
         }
     }
+
 }
 
 @Composable
-private fun Greeting(name: String,msg:String,id:Int) {
+private fun Greeting(name: String, msg: String, id: Int) {
     Card(
-        backgroundColor = MaterialTheme.colors.primary,
+        backgroundColor = colorResource(id = R.color.purple_200),
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        CardContent(name,msg,id)
+        CardContent(name, msg, id)
     }
 }
 
 @Composable
-private fun CardContent(name: String,msg:String,id:Int) {
+private fun CardContent(name: String, msg: String, id: Int) {
     var expanded by remember { mutableStateOf(false) }
     Row(
         modifier = Modifier
@@ -111,6 +151,7 @@ private fun CardContent(name: String,msg:String,id:Int) {
                 .weight(1f)
                 .padding(12.dp)
         ) {
+
             Text(text = id.toString())
             Text(
                 text = name,
