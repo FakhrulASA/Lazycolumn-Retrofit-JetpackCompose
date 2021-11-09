8b526ed0957b372c796fae198658091a5ffe3b8f
fakhrul <fakhrul.alam@btracsl.com>
2021-11-09 10:47:05 +0600

    -viewmodel added

--- app/src/main/java/com/fakhrulasa/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/fakhrulasa/jetpackcomposebasics/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.fakhrulasa.jetpackcomposebasics
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
@@ -11,22 +11,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.fakhrulasa.jetpackcomposebasics.network.PostData
 import com.fakhrulasa.jetpackcomposebasics.network.Retrofitlient
+import com.fakhrulasa.jetpackcomposebasics.ui.MainViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -34,37 +35,32 @@ import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     lateinit var postData: List<PostData>
+    val vm:MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        CoroutineScope(Dispatchers.IO).launch {
-            postData = Retrofitlient.api.getPost()
-            withContext(Dispatchers.Main) {
-                setContent {
-                    MyApp(postData)
-                }
+        vm.listData.observe(this,{
+            setContent {
+                MyApp(it)
             }
-
-        }
-
-
+        })
     }
 }
 
 @Composable
 private fun MyApp(postData: List<PostData>) {
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
-        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        OnboardingScreen(postData, onContinueClicked = { shouldShowOnboarding = false })
     } else {
         Greetings(postData)
     }
 }
 
 @Composable
-private fun OnboardingScreen(onContinueClicked: () -> Unit) {
+private fun OnboardingScreen(postData: List<PostData>, onContinueClicked: () -> Unit) {
     Surface {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -95,6 +91,28 @@ private fun OnboardingScreen(onContinueClicked: () -> Unit) {
                         text = "CHECKOUT POST",
                         color = Color.White
                     )
+                }
+                Card(
+                    backgroundColor = colorResource(id = R.color.teal_700),
+                    modifier = Modifier.padding(20.dp),
+                    shape = RoundedCornerShape(10.dp),
+                    elevation = 15.dp
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .padding(20.dp)
+                    ) {
+                        Text(
+                            text = "Total posts available",
+                            style = MaterialTheme.typography.h4.copy(
+                                fontWeight = FontWeight.ExtraBold
+                            ),
+                            fontSize = 18.sp
+                        )
+                        Text(text = postData.size.toString(), fontSize = 30.sp)
+                    }
                 }
             }
 
@@ -155,13 +173,16 @@ private fun CardContent(name: String, msg: String, id: Int) {
             Text(text = id.toString())
             Text(
                 text = name,
+                color = colorResource(id = R.color.dark_green),
                 style = MaterialTheme.typography.h4.copy(
                     fontWeight = FontWeight.ExtraBold
-                )
+                ),
+                fontSize = 18.sp
             )
             if (expanded) {
                 Text(
                     text = msg,
+                    color = colorResource(id = R.color.dark_green)
                 )
             }
         }
@@ -179,17 +200,3 @@ private fun CardContent(name: String, msg: String, id: Int) {
     }
 }
 
-@Preview(
-    showBackground = true,
-    widthDp = 320,
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "DefaultPreviewDark"
-)
-
-
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnboardingPreview() {
-    OnboardingScreen(onContinueClicked = {})
-
-}
